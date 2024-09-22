547fd1a3f3d78687f1e804d709d2dd7e1dab0fee
Ramdas Darwatkar <darwatkar.ram@gmail.com>
2024-09-22 20:16:24 +0530

    Updated UI

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-      navigator.serviceWorker.register('/serviceWorker.js')
+      navigator.serviceWorker.register('serviceWorker.js')
           .then(function(registration) {
               console.log('Service Worker registered with scope:', registration.scope);
           })
@@ -9,6 +9,7 @@ if ('serviceWorker' in navigator) {
           });
   });
 }
+
 
 
     const exerciseCanvas = document.getElementById("exerciseCanvas");
